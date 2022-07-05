aa2a0cb635380e7507c59835381cdfaacfe6eacc
Win10 <Win10@DESKTOP-QI2FVPP>
2022-07-05 16:20:01 +0530

    second commit

---
+++ Clock/src/MyFrame.java
@@ -0,0 +1,14 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("My Clock Program");
+		this.setLayout(new FlowLayout());
+		this.set
+	}
+
+}
+
